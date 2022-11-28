00fe88d3ce17705f15429013229c184f6f446813
nikileshkumar <nikileshkumar333@gmail.com>
2022-11-29 02:31:47 +0530

    swagger added

--- src/main/java/com/encora/departmet/DepartmentServiceApplication.java
+++ src/main/java/com/encora/departmet/DepartmentServiceApplication.java
@@ -1,48 +1,21 @@
 package com.encora.departmet;
 
-import com.encora.departmet.model.StudentDetails;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
-@RequestMapping("/department")
+@RefreshScope
+@EnableFeignClients
+@EnableSwagger2
 public class DepartmentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentServiceApplication.class, args);
+    }
 
-	private RestTemplate restTemplate = new RestTemplate();
-
-	@GetMapping("/hello")
-	public String hello(){
-		return "I am department service";
-	}
-
-	@GetMapping("/studentDetails/{departmentId}")
-	public StudentDetails getStudentDetails(@PathVariable("departmentId") String departmentId) {
-		StudentDetails listOfStudents = restTemplate.getForObject("http://STUDENT-SERVICE/student/" + departmentId, StudentDetails.class);
-		return listOfStudents;
-	}
-
-	@GetMapping("/departmentDetails/{studentName}")
-	public String getDepartmentDetails(@PathVariable("studentName") String studentName) {
-		if (studentName.equals("Rahul") || studentName.equals("Nikilesh")) {
-			return "employees";
-		} else if (studentName.equals("Thimmaiah") || studentName.equals("Kishore")) {
-			return "Managers";
-		}
-		return "StudentName not found";
-	}
 }
